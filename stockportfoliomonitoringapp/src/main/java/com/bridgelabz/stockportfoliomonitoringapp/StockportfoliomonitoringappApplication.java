bbb3114352833ecc34487c6400e4835d6b00b8af
fasfasdf <fdsaf@gvfds.com>
2025-05-26 11:51:14 +0530

    Initial commit on dev branch

---
+++ stockportfoliomonitoringapp/src/main/java/com/bridgelabz/stockportfoliomonitoringapp/StockportfoliomonitoringappApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.stockportfoliomonitoringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockportfoliomonitoringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockportfoliomonitoringappApplication.class, args);
+	}
+
+}
