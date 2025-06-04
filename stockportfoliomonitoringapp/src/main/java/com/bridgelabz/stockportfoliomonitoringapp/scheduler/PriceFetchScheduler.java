9c086a76366bf29a97b7d7d2dc85d9c5f8574a16
fasfasdf <fdsaf@gvfds.com>
2025-06-04 20:27:07 +0530

    Rollback to commit a0f2416

--- stockportfoliomonitoringapp/src/main/java/com/bridgelabz/stockportfoliomonitoringapp/scheduler/PriceFetchScheduler.java
+++ stockportfoliomonitoringapp/src/main/java/com/bridgelabz/stockportfoliomonitoringapp/scheduler/PriceFetchScheduler.java
@@ -1,6 +1,5 @@
-package com.bridgelabz.stockportfoliomonitoringapp.scheduler;
-
-public class PriceFetchScheduler {
-
-	
-}
+//package com.bridgelabz.stockportfoliomonitoringapp.scheduler;
+//
+//public class PriceFetchScheduler {
+//
+//}
--- stockportfoliomonitoringapp/src/main/java/com/bridgelabz/stockportfoliomonitoringapp/serviceimpl/AlertServiceImpl.java
+++ stockportfoliomonitoringapp/src/main/java/com/bridgelabz/stockportfoliomonitoringapp/serviceimpl/AlertServiceImpl.java
@@ -30,25 +30,25 @@ import com.bridgelabz.stockportfoliomonitoringapp.util.StocksApi;
 
 @Service
 public class AlertServiceImpl implements AlertService{
-
+	
 	@Autowired
 	AlertRepository alertRepository;
-
+	
 	@Autowired
 	UserRepository userRepository;
-
+	
 	@Autowired
-	JavaMailSender mailSender;
-
+    JavaMailSender mailSender;
+	
 	@Autowired
 	MailServiceImpl mailServiceImpl;
-
+	
 	@Autowired
 	StocksApi stocksApi;
-
+	
 	//set alert
 	public AlertPostResponseDto setAlert(long userId, AlertPostRequestDto alertPostRequestDto) {
-
+		
 		Optional<User> user = userRepository.findById(userId);
 		if(user.isEmpty()){
 			throw new UserNotFoundException("User does not exist");
@@ -59,97 +59,97 @@ public class AlertServiceImpl implements AlertService{
 		alert.setPortfolioLossPercentage(0);
 		alert.setTriggered(false);
 		alert.setUser(user.get());
-
+		
 		Alert alertSaved = alertRepository.save(alert);
-
+		
 		AlertPostResponseDto alertPostResponseDto = new AlertPostResponseDto(alertSaved.getStockSymbol(), alertSaved.getPriceThreshold());
 		return alertPostResponseDto;
 	}
-
-
+	
+	
 	//Fetch Alerts
 	public List<AlertGetResponseDto> getAllAlert(long userId) {
-		Optional<User> user = userRepository.findById(userId);
-		if (user.isEmpty()) {
-			throw new UserNotFoundException("User does not exist");
-		}
+	    Optional<User> user = userRepository.findById(userId);
+	    if (user.isEmpty()) {
+	        throw new UserNotFoundException("User does not exist");
+	    }
 
-		List<Alert> alertList = alertRepository.findByUserUserId(userId);
-		List<AlertGetResponseDto> alertGetResponseDtoList = new ArrayList<>();
+	    List<Alert> alertList = alertRepository.findByUserUserId(userId);
+	    List<AlertGetResponseDto> alertGetResponseDtoList = new ArrayList<>();
 
-		for (Alert x : alertList) {
-			AlertGetResponseDto alertGetResponseDto = new AlertGetResponseDto(); // Moved inside loop
-			alertGetResponseDto.setAlertId(x.getAlertId());
-			alertGetResponseDto.setStockSymbol(x.getStockSymbol());
-			alertGetResponseDto.setPriceThreshold(x.getPriceThreshold());
-			alertGetResponseDto.setPortfolioLossPercentage(x.getPortfolioLossPercentage());
-			alertGetResponseDto.setTriggered(x.isTriggered());
-			alertGetResponseDtoList.add(alertGetResponseDto);
-		}
+	    for (Alert x : alertList) {
+	        AlertGetResponseDto alertGetResponseDto = new AlertGetResponseDto(); // Moved inside loop
+	        alertGetResponseDto.setAlertId(x.getAlertId());
+	        alertGetResponseDto.setStockSymbol(x.getStockSymbol());
+	        alertGetResponseDto.setPriceThreshold(x.getPriceThreshold());
+	        alertGetResponseDto.setPortfolioLossPercentage(x.getPortfolioLossPercentage());
+	        alertGetResponseDto.setTriggered(x.isTriggered());
+	        alertGetResponseDtoList.add(alertGetResponseDto);
+	    }
 
-		return alertGetResponseDtoList;
+	    return alertGetResponseDtoList;
 	}
-
+	
 	//update alert
 	public Alert updateAlert(long alertId, AlertUpdateRequestDto alertUpdateRequestDto) {
 		Optional<Alert> optionalAlert = alertRepository.findById(alertId);
 		if (optionalAlert.isEmpty()) {
-			throw new UserNotFoundException("User does not exist");
-		}
-
+	        throw new UserNotFoundException("User does not exist");
+	    }
+		
 		Alert alert = optionalAlert.get();
-
+		
 		if(alertUpdateRequestDto.getPortfolioLossPercentage() != null) {
 			alert.setPortfolioLossPercentage(alertUpdateRequestDto.getPortfolioLossPercentage());
 		}
-
+		
 		if(alertUpdateRequestDto.getPriceThreshold() != null) {
 			alert.setPriceThreshold(alertUpdateRequestDto.getPriceThreshold());
 		}
-
+		 
 		Alert updatedAlert = alertRepository.save(alert);
-
+		
 		return updatedAlert;
 	}
-
-
+	
+	
 	//Sending alertMail to any User
-	//	@Scheduled(fixedDelay = 2000)
+//	@Scheduled(fixedDelay = 2000)
 	public void alertMail() {
-		List<Alert> log = alertRepository.findAll();
+	    List<Alert> log = alertRepository.findAll();
 
-		for (Alert alert : log) {
-			try {
-				if (alert.isTriggered()) continue; 
+	    for (Alert alert : log) {
+	        try {
+	            if (alert.isTriggered()) continue; 
 
-				double currentPrice = stocksApi.fetchStockPrice(alert.getStockSymbol());
-				double boughtPrice = alert.getPriceThreshold();
+	            double currentPrice = stocksApi.fetchStockPrice(alert.getStockSymbol());
+	            double boughtPrice = alert.getPriceThreshold();
 
-				if (currentPrice > boughtPrice) {
-					User user = alert.getUser();
-					String body = "Alert! The price of " + alert.getStockSymbol() + 
-							" is now ₹" + currentPrice + 
-							", above your threshold ₹" + boughtPrice;
-					sendEmail(user.getEmail(), "Stock Alert Triggered!", body);
-
-					alert.setTriggered(true);
-					alertRepository.save(alert);
-				}
-			} catch (Exception e) {
-				// Log error
-				System.err.println("Error processing alert for " + alert.getStockSymbol());
-			}
-		}
+	            if (currentPrice > boughtPrice) {
+	                User user = alert.getUser();
+	                String body = "Alert! The price of " + alert.getStockSymbol() + 
+	                              " is now ₹" + currentPrice + 
+	                              ", above your threshold ₹" + boughtPrice;
+	                sendEmail(user.getEmail(), "Stock Alert Triggered!", body);
+	                
+	                alert.setTriggered(true);
+	                alertRepository.save(alert);
+	            }
+	        } catch (Exception e) {
+	            // Log error
+	            System.err.println("Error processing alert for " + alert.getStockSymbol());
+	        }
+	    }
 	}
 
-
+	
 	public void sendEmail(String to, String subject, String body) {
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom("naidusp555@gmail.com");  
-		message.setTo(to);
-		message.setSubject(subject);
-		message.setText(body);
-		mailSender.send(message);
-	}
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("naidusp555@gmail.com");  
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
 
 }
